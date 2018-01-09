46a73f569c73b9c772015d744f241ecd331ef782
Kwon <Kwon@DESKTOP-CPH25AC>
2018-01-09 13:14:55 +0900

    매서드 수정 & 매서드 추가 & 클래스 주석 수정
    1. Location 클래스의 설명 변경
    2. instanceAt매서드 내용 변경 -> 인스턴스 생성 안돼있을 때 초기화 해준 구조에서 static구간에서 초기화해주는
    구조로 변경
    3. getter 매서드 제거 후 멤버변수 상수로 변경 후 외부로 공개
    4. 모든 좌표를 반환하는 values매서드 추가
    5. 장기판 내에서 상하좌우반전 시킨 위치를 반환하는 getAwayLocation매서드 추가

--- ChineseChess/src/com/kms/chinesechess/support/Location.java
+++ ChineseChess/src/com/kms/chinesechess/support/Location.java
@@ -1,51 +1,59 @@
 package com.kms.chinesechess.support;
 
 /**
- * 장기판 안에서 가질 수 있는 모든 좌표 정보를 제공해주는 클래스
+ * 장기판 안에서의 좌표 정보를 제공해주는 클래스
  * @author Kwon
  *
  */
 public class Location {
-	
 	public static final int MIN_ROW = 1, MIN_COL = 1;
 	public static final int MAX_ROW = 10, MAX_COL = 9;
 	
-	private int row, col;
+	public final int ROW, COL;
 	
 	private static final Location[][] INSTANCE = new Location[MAX_ROW + 1][MAX_COL + 1];
 	
+	static {
+		for( int row = 1; row <= MAX_ROW; row++ ) 
+			for( int col = 1; col <= MAX_COL; col++ ) 
+				INSTANCE[row][col] = new Location(row, col);
+	}
+	
 	private Location( int row, int col ) {
-		
-		this.row = row;
-		this.col = col;
-		
+		this.ROW = row;
+		this.COL = col;
 	}
 	/**
 	 * 원하는 좌표 정보를 가지고 있는 INSTANCE를 반환해주는 매서드
-	 * @param row
-	 * @param col
+	 * @param row MIN_ROW ~ MAX_ROW까지의 row
+	 * @param col MIN_COL ~ MAX_COL까지의 col
 	 * @return INSTANCE[row][col]
 	 */
 	public static Location instanceAt( int row, int col ) {
-		
 		if( row > MAX_ROW || col > MAX_COL || row < MIN_ROW || col < MIN_COL )	return null;
 		
-		if( INSTANCE[row][col] != null )	return INSTANCE[row][col];
-		
-		return INSTANCE[row][col] = new Location( row, col );
-		
+		return INSTANCE[row][col];
 	}
-	
-	public int getRow() {
+	/**
+	 * 장기판에서의 모든 좌표를 반환하는 매서드
+	 * @return 장기판에서의 모든 좌표
+	 */
+	public static Location[][] values() {
+		Location[][] values = new Location[MAX_ROW][MAX_COL];
 		
-		return row;
+		for(int row = 0; row < MAX_ROW; row++) {
+			for(int col = 0; col < MAX_COL; col++) {
+				values[row][col] = INSTANCE[row + 1][col + 1];
+			}
+		}
 		
+		return values;
 	}
-	
-	public int getCol() {
-		
-		return col;
-		
+	/**
+	 * 장기판에서 본 위치에서 상하좌우 반전시킨 위치를 반환하는 매서드 
+	 * @return 장기판에서 본 위치에서 상하좌우 반전시킨 위치
+	 */
+	public Location getAwayLocation() {
+		return instanceAt( MAX_ROW + MIN_ROW - ROW, MAX_COL + MIN_COL - COL );
 	}
-	
 }
